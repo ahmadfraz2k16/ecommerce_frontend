5a61d96ba9283a46f48584544c12f563a90ca813
ahmadfraz2k16 <ahmadfraz2k16@gmail.com>
2023-03-15 02:35:33 +0500

    created redux-store 4(1,2,3,4,5)
    Overview redux, creating redux store ( lesson 1, 2)
    Product List, Reducer & Action ( lesson 3)
    Bringing Redux to HomeScreen ( lesson 4)
    Message and Loader component ( lesson 5)

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,23 +2,34 @@ import React, {useState, useEffect} from 'react'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
 import axios from 'axios'
+import {useDispatch, useSelector} from 'react-redux'
+import {listProducts} from '../actions/productActions'
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+
 function HomeScreen() {
   // products is a state and setProducts is a method which will set the state = products, 
   // by default state will be set to empty Array, then fetchProducts() will change the state 
-  const [products, setProducts] = useState([])
-
+  // const [products, setProducts] = useState([])
+const dispatch = useDispatch()
+const productList = useSelector(state => state.productList)
+const { error, loading, products} = productList
 useEffect(()=>{
-    async function fetchProducts(){
-      const {data} = await axios.get('/api/products/')
-      setProducts(data)
-    }
-    fetchProducts()
-},[])
+  dispatch(listProducts())
+    // async function fetchProducts(){
+    //   const {data} = await axios.get('/api/products/')
+    //   setProducts(data)
+    // }
+    // fetchProducts()
+},[dispatch])
 
   return (
     <div>
         <h1>Latest Products</h1>
-        <Row>
+        {loading ? <Loader />
+                : error ? <Message variant='danger'>{error}</Message>
+                    :
+                    <Row>
             {products.map(product => (
                 // giving unique id for each product, otherwise error of key in console
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -27,6 +38,8 @@ useEffect(()=>{
                 </Col>
             ))}
         </Row>
+  }           
+        
     </div>
   )
 }
