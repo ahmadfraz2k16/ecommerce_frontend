e407bb627535c72645063529403ed817556b228c
ahmadfraz2k16 <ahmadfraz2k16@gmail.com>
2023-04-01 13:12:54 +0500

    created cart screen view 5(4)
    cart items shown with variable quantity option
    delete item button shown
    price updates as quantity + or -
    if no items then proceed to checkout btn disabled

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -13,17 +13,107 @@ function CartScreen({ match, location, history }) {
   console.log('Product ID is :', productId)
   // Extract quantity from the URL query string
   const qty = new URLSearchParams(useLocation().search).get('qty');
+
+  const navigate = useNavigate();
+  const handleProceedToCheckout = () => {
+    navigate('/shipping')
+  }
   const dispatch = useDispatch();
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+  console.log('cart items :', cartItems)
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId, qty])
 
-   return (
+  const itemsTotal = cartItems.reduce((acc, item) => acc + parseInt(item.qty), 0);
+
+  const subtotal = cartItems.reduce((acc, item) => {
+    const price = Number(item.price);
+    const qty = Number(item.qty);
+    if (isNaN(price) || isNaN(qty)) {
+      return acc;
+    }
+    return acc + price * qty;
+  }, 0).toFixed(2);
+  console.log(itemsTotal)
+  console.log(subtotal)
+
+  return (
     <Row>
-      cart   
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message variant='info'>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map(item => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+
+                  <Col md={2}>
+                    ${item.price}
+                  </Col>
+
+                  <Col md={3}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
+                    >
+                      {
+
+                        [...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))
+                      }
+
+                    </Form.Control>
+                  </Col>
+
+                  <Col md={1}>
+                    <Button
+                      type='button'
+                      variant='light'>
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+
+
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <ListGroup variant='flush'>
+          <ListGroup.Item>
+            <h2>Subtotal ({cartItems.reduce((acc, item) => acc + parseInt(item.qty), 0)}) items
+            </h2>
+            <h3 className='text-right'>${subtotal}</h3>
+
+          </ListGroup.Item>
+          <ListGroup.Item className='text-right'>
+            <Button disabled={cartItems.length === 0} onClick={handleProceedToCheckout} className='btn-block'>Proceed to Checkout</Button>
+          </ListGroup.Item>
+        </ListGroup>
+      </Col>
+
+
     </Row>
-  ); 
+  );
 }
 export default CartScreen;
